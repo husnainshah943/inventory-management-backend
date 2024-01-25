2644f888bebcad7da2d0c039dc424fcbe407ac03
Husnain Shah <husnainamin44@gmail.com>
2024-01-25 21:40:08 +0500

    update

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const salesRoute = require("./router/sales");
 const cors = require("cors");
 const User = require("./models/users");
 const Product = require("./models/Product");
-
+require("dotenv").config();
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 8080;
 main();
 app.use(express.json());
 app.use(cors());
